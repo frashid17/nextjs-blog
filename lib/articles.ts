a0b4a96a47e00f6c3f7af7f445dc825932624e2c
frashid17 <frashid274@gmail.com>
2024-09-28 16:23:56 +0300

    exported articles data

--- lib/articles.ts
+++ lib/articles.ts
@@ -1,43 +1,54 @@
-import fs from "fs"
-import matter from "gray-matter"
-import path from "path"
-import moment from "moment"
-import { remark } from "remark"
-import html from "remark-html"
+import fs from "fs";
+import matter from "gray-matter";
+import path from "path";
+import moment from "moment";
 
-import type { ArticleItem } from "@/types"
+import type { ArticleItem } from "@/types";
 
-const articlesDirectory = path.join(process.cwd(), "articles")
+const articlesDirectory = path.join(process.cwd(), "articles");
 
 const getSortedArticles = (): ArticleItem[] => {
-  const fileNames = fs.readdirSync(articlesDirectory)
+    const fileNames = fs.readdirSync(articlesDirectory);
 
-  const allArticlesData = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.md$/, "")
+    const allArticlesData = fileNames.map((fileName) => {
+        const id = fileName.replace(/\.md$/, "");
 
-    const fullPath = path.join(articlesDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, "utf-8")
+        const fullPath = path.join(articlesDirectory, fileName);
+        const fileContents = fs.readFileSync(fullPath, "utf-8");
 
-    const matterResult = matter(fileContents)
+        const matterResult = matter(fileContents);
 
-    return {
-      id,
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      category: matterResult.data.category,
-    }
-  })
+        return {
+            id,
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            category: matterResult.data.category,
+        };
+    });
 
-  return allArticlesData.sort((a, b) => {
-    const format = "DD-MM-YYYY"
-    const dateOne = moment(a.date, format)
-    const dateTwo = moment(b.date, format)
-    if (dateOne.isBefore(dateTwo)) {
-      return -1
-    } else if (dateTwo.isAfter(dateOne)) {
-      return 1
-    } else {
-      0
-    }
-  })
-}
+    return allArticlesData.sort((a, b) => {
+        const format = "DD-MM-YYYY";
+        const dateOne = moment(a.date, format);
+        const dateTwo = moment(b.date, format);
+        if (dateOne.isBefore(dateTwo)) {
+            return -1;
+        } else if (dateTwo.isAfter(dateOne)) {
+            return 1;
+        }
+        return 0;
+    });
+};
+
+export const getCategorisedArticles = () => {
+    const sortedArticles = getSortedArticles();
+    const categorizedArticles: { [key: string]: ArticleItem[] } = {};
+
+    sortedArticles.forEach((article) => {
+        if (!categorizedArticles[article.category]) {
+            categorizedArticles[article.category] = [];
+        }
+        categorizedArticles[article.category].push(article);
+    });
+
+    return categorizedArticles;
+};
