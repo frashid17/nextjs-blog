1494e469c62381158e0abd976d45c618138f6b75
frashid17 <frashid274@gmail.com>
2024-09-28 17:18:49 +0300

    Added hover:shadow-xl to the article items to create a more pronounced shadow

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ const HomePage = () => {
   return (
     <section className="mx-auto w-11/12 md:w-4/5 mt-20 flex flex-col gap-16 mb-20">
       <header className="font-cormorantGaramond font-light text-6xl text-neutral-900 text-center">
-        <h1>Patrick's Blog</h1>
+        <h1>Patrick&apos;s Blog</h1> {/* Escaped apostrophe */}
       </header>
       <section className="md:grid md:grid-cols-2 gap-10">
         {articles !== null &&
           Object.keys(articles).map((article) => (
             <div
               key={article}
-              className="bg-white rounded-lg shadow-lg p-5 transition-transform transform hover:scale-105"
+              className="bg-white rounded-lg shadow-lg p-6 transition-transform transform hover:scale-105 hover:shadow-xl"
             >
               <ArticleItemList
                 category={article}
